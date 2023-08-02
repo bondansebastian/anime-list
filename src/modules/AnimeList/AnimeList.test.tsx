daa353d34dcd0e850735d71a7528b7ffd4ac9e71
Bondan Sebastian <bondansebastian@gmail.com>
2023-08-02 18:05:50 +0700

    AN-4 Implemented anime list page test

--- src/modules/AnimeList/AnimeList.test.tsx
+++ src/modules/AnimeList/AnimeList.test.tsx
@@ -2,9 +2,45 @@ import React from 'react';
 import { render, screen } from '@testing-library/react';
 import AnimeList from './AnimeList';
 import { BrowserRouter } from 'react-router-dom';
+import { MockedProvider } from "@apollo/client/testing";
+import { GET_PAGE } from '../../queries';
 
-test('renders Home text', () => {
-  render(<AnimeList />, { wrapper: BrowserRouter });
-  const text = screen.getByText(/Home/i);
-  expect(text).toBeInTheDocument();
+const mocks = [
+  {
+    request: {
+      query: GET_PAGE,
+      variables: {
+        page: 1
+      }
+    },
+    result: {
+      data: {
+        Page: {
+          media: [
+            { 
+              id: 1, 
+              title: { english: 'Test Anime', userPreferred: 'Test Anime' },
+              coverImage: { medium: '', large: '' } 
+            }
+          ],
+          pageInfo: {
+            perPage: 10,
+            currentPage: 1,
+            hasNextPage: false,
+          }
+        }
+      }
+    }
+  }
+]
+
+test('renders without error', async () => {
+  render(
+    <MockedProvider mocks={mocks} addTypename={false}>
+      <AnimeList />
+    </MockedProvider>,
+    { wrapper: BrowserRouter }
+  );
+  expect(await screen.findByText('Loading')).toBeInTheDocument();
+  expect(await screen.findByText("Test Anime")).toBeInTheDocument();
 });
